476aaa05c8d3ebd04b8396aed7bc6e52ccee137d
Winlex1 <113421468+Winlex1@users.noreply.github.com>
2022-10-13 08:45:30 +0200

    Update StringTools.cs

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +6,28 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            int x = 0;
+            for(int i = 0; i < strings.Length; i++)
+            {
+                if(strings[i].Length == 4)
+                {
+                    x++;
+                }
+                
+            }
+
+           int y = 0;
+            string[] strings2 = new string[x];
+            for(int j = 0; j < strings.Length; j++)
+            {
+                if(strings[j].Length == 4)
+                {
+                    strings2[y] = strings[j]; 
+                    y++;
+                }
+                
+            }
+            return strings2;
         }
     }
 }
